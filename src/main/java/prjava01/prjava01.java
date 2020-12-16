4c480452dac5def08d43255e003f16e296dbdbd0
sesigi2021daw2@protonmail.com <sergi@DESKTOP-QUBGHPF>
2020-12-16 19:55:42 +0100

    ersió 0.2 del projecte prjava01

---
+++ src/main/java/prjava01/prjava01.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+
+import java.io.*;
+public class prjava01 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+		bw.write("      Nova p&agrave;gina  web");bw.newLine();
+		bw.write("    </title>");bw.newLine();
+		bw.write("  </head>");bw.newLine();
+		bw.write("  <body>");bw.newLine();
+                bw.write(" <h1>DAW2 m08uf4eac1</h1>");bw.newLine(); 
+		bw.write("    Nova p&agrave;gina web");bw.newLine();
+		bw.write("  </body>");bw.newLine();
+		bw.write("</html>");bw.newLine();
+		bw.close();
+		} }
+	}
